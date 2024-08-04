76e0608088c6ca305c46ea3881962c5e5c1b698e
Mehdi Chikhi <ci_mi@outlook.fr>
2024-08-04 09:06:00 +0100

    first commit

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,15 +7,50 @@
 
 import SwiftUI
 
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle.bold())
+            .foregroundStyle(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(.rect(cornerRadius: 15))
+    }
+}
+
+extension View {
+    func titleStyle () -> some View {
+        modifier(Title())
+    }
+}
+
+struct Watermark: ViewModifier {
+    let title: String
+
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+
+            Text(title)
+                .font(.headline.weight(.semibold))
+                .foregroundStyle(.white)
+                .padding(5)
+                .background(.secondary)
+        }
+    }
+}
+
+extension View {
+    func watermarkStyle (with text: String) -> some View {
+        modifier(Watermark(title: text))
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Hello World")
+            .titleStyle()
+            .watermarkStyle(with: "test")
     }
 }
 
